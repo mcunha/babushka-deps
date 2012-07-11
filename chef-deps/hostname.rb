42f120ef204448f726f4ebea528f6bcb07b1d36e
Marco Cunha <marco.cunha@gmail.com>
2012-07-11 18:58:09 +0200

    fix for hostname vars changes

--- chef-deps/hostname.rb
+++ chef-deps/hostname.rb
@@ -1,14 +1,15 @@
 dep 'hostname', :hostname_str, :for => :linux do
-  def hostname
-    hostname_str.default(shell('hostname -f'))
+  hostname_str.default(shell('hostname -f'))
+  def myhostname
+    hostname_str
   end
   met? {
     stored_hostname = '/etc/hostname'.p.read
     !stored_hostname.blank? && hostname == stored_hostname
   }
   meet {
-    sudo "echo #{hostname_str.default(shell('hostname -f'))} > /etc/hostname"
-    sudo "sed -ri 's/^127.0.0.1.*$/127.0.0.1 #{hostname_str} #{hostname_str.sub(/\..*$/, '')} localhost.localdomain localhost/' /etc/hosts"
-    sudo "hostname #{hostname_str}"
+    sudo "echo #{myhostname} > /etc/hostname"
+    sudo "sed -ri 's/^127.0.0.1.*$/127.0.0.1 #{myhostname} #{myhostname.sub(/\..*$/, '')} localhost.localdomain localhost/' /etc/hosts"
+    sudo "hostname #{myhostname}"
   }
 end
